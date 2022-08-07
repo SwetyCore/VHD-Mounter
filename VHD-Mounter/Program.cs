6cd89293febb5d3fa6c85aa968653668b1fd5c7f
Swety-PC <Swety-PC@DESKTOP-UPOF2NG>
2022-08-07 21:30:20 +0800

    添加项目文件。

---
+++ VHD-Mounter/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Diagnostics;
+
+const string VHDX_FILE = @"D:\VHD_Mount\应用数据.vhdx";
+
+Process p = new();
+p.StartInfo.FileName = "diskpart";
+p.StartInfo.CreateNoWindow = true;
+p.StartInfo.UseShellExecute = false;
+p.StartInfo.RedirectStandardOutput = true;
+p.StartInfo.RedirectStandardInput = true;
+p.Start();
+
+p.StandardInput.WriteLine($"select vdisk file=\"{VHDX_FILE}\"");
+p.StandardInput.WriteLine("attach vdisk");
+p.StandardInput.WriteLine("exit");
+p.WaitForExit();
